5f6bde686eda23f0722404721f53bc457f820cad
Tim Colpus <Tim.Colpus@softwire.com>
2018-07-05 09:47:44 +0100

    just added parserXML

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const logger = log4js.getLogger('index.js');
 
 // dependencies
 var moment = require('moment');
+var readline = require('readline-sync');
 
 // custom classes
 var account = require("./account");
@@ -24,63 +25,74 @@ var parserJSON = require("./parserJSON");
 let parCSV = new parserCSV();
 let parJSON = new parserJSON();
 
-// set up accounts
+// set up accounts and transfers
 var accounts = new Map();
+var transfers = [];
 
-
+// returns list of accounts calculated from a list of transfers
 function setupAccounts (transfers) {
-    let accounts = new Map();
+    let a = new Map();
     // apply transfers to accounts (making new accounts in the process)
     transfers.forEach(function(t) {
-        if (!accounts[t.from]) {
-            accounts[t.from] = new account(t.from);
+        if (!a[t.from]) {
+            a[t.from] = new account(t.from);
         }
-        if (!accounts[t.to]) {
-            accounts[t.to] = new account(t.to);
+        if (!a[t.to]) {
+            a[t.to] = new account(t.to);
         }
-        accounts[t.from].applyTransfer(t);
-        accounts[t.to].applyTransfer(t);
+        a[t.from].applyTransfer(t);
+        a[t.to].applyTransfer(t);
     })
-    return accounts;
+    return a;
 }
 
 
 // main
-process.stdin.on('readable', () => {
-    const input = process.stdin.read().toString("utf-8").slice(0,-1);
+while(true) {
+    var input = readline.question("Enter command: ");
+    if (input == "Exit") {
+        break;
+    }
     if (input !== null) {
-        if ( input.startsWith("List ") ) {
+        if (input.startsWith("List ")) {
             var x = input.substring("List ".length);
             if (x == "All") {
-                for (let name in accounts) {
-                    console.log(name + "   " + accounts[name].balance.toFixed(2));
+                if (accounts !== {}) {
+                    for (let name in accounts) {
+                        console.log(name + "   " + accounts[name].balance.toFixed(2));
+                    }
+                } else {
+                    console.log("No accounts found");
                 }
             } else if (!accounts[x]) {
                 console.log("Error: account does not exist");
             } else {
                 console.log(accounts[x].statement.join("\n"));
             }
-        } else if ( input.startsWith("Import ") ) {
-            var file = input.split(" ")[1];
-            filename = file.split(".")[0];
-            fileformat = file.split(".")[1];
-            console.log(file);
+        } else if (input.startsWith("Import ")) {
+            var file = input.split(' ')[1];
+            // var filename = file.split('.')[0];
+            var fileformat = file.split('.')[1];
             switch (fileformat) {
                 case "csv":
-                    var transfers = parCSV.getTransfers(file);
+                    transfers = parCSV.getTransfers(file);
+                    console.log("csv file loaded");
                     break;
                 case "json":
-                    var transfers = parJSON.getTransfers(file);
+                    transfers = parJSON.getTransfers(file);
+                    console.log("json file loaded");
                     break;
                 case "xml":
+                    console.log("xml file loaded");
                     break;
                 default:
                     console.log("File format not supported");
             }
+            accounts = setupAccounts(transfers);
         } else {
             logger.error("Cheeeeese");
             console.log("instruction not understood");
         }
     }
-});
+}
 
